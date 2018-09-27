1c231cb7484cbf5f9620b9041d3583adf1d6d1c0
Mark Lamourine <markllama@gmail.com>
2018-09-26 23:08:26 -0400

    added ybias to create a rectangle and started adding scrolls to canvas

--- src/components/HexmapCanvas.tsx
+++ src/components/HexmapCanvas.tsx
@@ -6,22 +6,31 @@ import ColoredHex from './ColoredHex';
 
 import HexVector from '../lib/hexmap/hexvector';
 
+import "./HexmapCanvas.css";
+
 interface IHexmapCanvasProps {
+  size: HexVector
+  origin: HexVector,
   hexrun: number,
-  origin: HexVector
 }
 
 class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
 
   public static propTypes = {
     hexrun: PropTypes.number,
-    origin: HexVector
+    origin: HexVector,
+    size: HexVector
   }
 
   public static defaultProps = {
     hexrun: 15,
-    origin: new HexVector()
+    origin: new HexVector(),
+    size: new HexVector(15, 21),
   }
+
+  public get hexradius() { return this.props.hexrun * 2; }
+  public get hexrise() { return Math.floor(this.hexradius * Math.sqrt(2/3)) }
+  public get hexheight() { return Math.floor(this.hexrise * 2) }
 
   public render() {
     
@@ -38,16 +47,11 @@ class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
 
   }
 
-//  private yBias(x:number) {
-//    Math.floor(x / 2)
-//  }
+  private yBias(x:number) { return Math.floor(x / 2) }
 
   private fill_map() {
 
-    const hexradius = this.props.hexrun * 2;
-    const hexrise = Math.floor(hexradius * Math.sqrt(2/3));
-    const hexheight = Math.floor(hexrise * 2);
-    const origin = new HexVector(hexradius + 10, hexheight);
+    const origin = new HexVector(this.hexradius + 10, this.hexheight);
     // const rowShift = new HexVector(0, -hexheight)
     const colShift = new HexVector(this.props.hexrun * 3, 0)
     
@@ -56,11 +60,12 @@ class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
     let row = 0
     let location: HexVector;
 
-    for (col = 0 ; col < 7 ; col++) {
-      for (row = 0 ; row < 10 ; row++) {
-        location = origin.add(colShift.mul(col)).add(new HexVector(0, (row * hexheight) - (col * hexrise)))
+    for (col = 0 ; col < this.props.size.hx ; col++) {
+      const bias = this.yBias(col);
+      for (row = 0 ; row < this.props.size.hy ; row++) {
+        location = origin.add(colShift.mul(col)).add(new HexVector(0, ((row + bias) * this.hexheight) - (col * this.hexrise)))
         // .add(yBias)
-        rows.push(<ColoredHex radius={hexradius} origin={location}/>);
+        rows.push(<ColoredHex radius={this.hexradius} origin={location}/>);
       }
     }
     return rows
