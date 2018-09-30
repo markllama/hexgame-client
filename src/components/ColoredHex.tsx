4745fc2a90d08874cc96ee9e96683535b49fe9ca
Mark Lamourine <markllama@gmail.com>
2018-09-29 20:47:19 -0400

    finally got the stage and hex layout right again

--- src/components/ColoredHex.tsx
+++ src/components/ColoredHex.tsx
@@ -6,19 +6,19 @@ import { RegularPolygon } from 'react-konva';
 import HexVector from '../lib/hexmap/hexvector';
 
 interface IColoredHexProps {
-  radius: number,
-  origin: HexVector
+  hexrun: number,
+  location: HexVector
 }
 
 class ColoredHex extends React.Component<IColoredHexProps, any> {
 
   public static propTypes = {
-    origin: HexVector,
-    radius: PropTypes.number
+    hexrun: PropTypes.number,
+    location: HexVector
   }
 
   public static defaultProps = {
-    radius: 15
+    hexrun: 15
   }
   
   public state = {
@@ -28,15 +28,44 @@ class ColoredHex extends React.Component<IColoredHexProps, any> {
   constructor(props: IColoredHexProps) {
     super(props)
   }
+
+  public get origin() {
+    return new HexVector(
+      this.props.hexrun * 2,
+      this.props.hexrun * 4 * Math.sqrt(2/3)
+    )
+  }
   
+  // pixel location of the hex on the canvas
+  public get center() {
+    const hexrise = this.props.hexrun * 2 * Math.sqrt(2/3)
+    const rowShift = new HexVector(0, hexrise * 2)
+    const colShift = new HexVector(this.props.hexrun * 3, 0)
+    const sawtooth = (this.location.hx % 2) === 0 ?
+      new HexVector() :
+      new HexVector(0, - hexrise)
+    
+    return this.origin
+      .add(colShift.mul(this.location.hx))
+      .add(rowShift.mul(this.location.hy))
+      .add(sawtooth)
+  }
+
+  public get location() {
+    return this.props.location
+  }
+
   // 
   public render() {
+
+    // calculate the pixel location for the hex
+    const p = this.center
     return (
         <RegularPolygon
-      x={this.props.origin.hx}
-      y={this.props.origin.hy}
+      x={p.hx}
+      y={p.hy}
       sides={6}
-      radius={this.props.radius}
+      radius={this.props.hexrun * 2}
       rotation={30}
       stroke={'1px'}
       fill={this.state.color}
--- src/components/HexmapCanvas.tsx
+++ src/components/HexmapCanvas.tsx
@@ -28,7 +28,9 @@ class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
     size: new HexVector(15, 21),
   }
 
-  public get hexradius() { return this.props.hexrun * 2; }
+  public get size() { return this.props.size; }
+  public get hexrun() { return this.props.hexrun; }
+  public get hexradius() { return this.hexrun * 2; }
   public get hexrise() { return Math.floor(this.hexradius * Math.sqrt(2/3)) }
   public get hexheight() { return Math.floor(this.hexrise * 2) }
 
@@ -36,7 +38,7 @@ class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
     
     return (
         <div className="HexmapCanvas">
-        <Stage width={this.props.hexrun * (this.props.size.hx*3+1)} height={this.hexrise * (this.props.size.hy*2+1)} >
+        <Stage width={this.hexrun * (this.size.hx*3+1)} height={this.hexrise * ((this.size.hy + 1)*2)} >
         <Layer>
         <Text text="Try click on rect" />
         {this.fill_map()}
@@ -47,25 +49,27 @@ class HexmapCanvas extends React.Component<IHexmapCanvasProps, any> {
 
   }
 
-  private yBias(x:number) { return Math.floor(x / 2) }
+  // private yBias(x:number) { return Math.floor(x / 2) }
 
   private fill_map() {
 
-    const origin = new HexVector(this.hexradius, this.hexheight);
+    // const origin = new HexVector(this.hexradius, this.hexheight);
     // const rowShift = new HexVector(0, -hexheight)
-    const colShift = new HexVector(this.props.hexrun * 3, 0)
+    // const colShift = new HexVector(this.props.hexrun * 3, 0)
     
     const rows = []
     let col = 0
     let row = 0
     let location: HexVector;
 
-    for (col = 0 ; col < this.props.size.hx ; col++) {
-      const bias = this.yBias(col);
-      for (row = 0 ; row < this.props.size.hy ; row++) {
-        location = origin.add(colShift.mul(col)).add(new HexVector(0, ((row + bias) * this.hexheight) - (col * this.hexrise)))
+    for (col = 0 ; col < this.size.hx ; col++) {
+      // const bias = this.yBias(col);
+      for (row = 0 ; row < this.size.hy ; row++) {
+        // location = origin.add(colShift.mul(col)).add(new HexVector(0, ((row + bias) * this.hexheight) - (col * this.hexrise)))
         // .add(yBias)
-        rows.push(<ColoredHex radius={this.hexradius} origin={location}/>);
+        // location = new HexVector(col, row + bias)
+        location = new HexVector(col, row)
+        rows.push(<ColoredHex hexrun={this.hexrun} location={location}/>);
       }
     }
     return rows
