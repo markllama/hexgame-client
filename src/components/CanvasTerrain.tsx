59fa68661833abf6f8b1799a431634c8b5d0ada6
Mark Lamourine <markllama@gmail.com>
2018-10-04 21:09:34 -0400

    settle Terrain files so they compile

---
+++ src/components/CanvasTerrain.tsx
@@ -0,0 +1,29 @@
+// import * as Konva from  'konva';
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+import { Group, Text } from 'react-konva';
+
+import HexVector from '../lib/hexmap/hexvector';
+import Terrain from '../lib/hexmap/terrain';
+
+interface ICanvasTerrain {
+  locations: HexVector[],
+  terrain: Terrain
+}
+
+class CanvasTerrain extends React.Component<ICanvasTerrain, any> {
+  public static propTypes = {
+    locations: PropTypes.array,
+    terrain: PropTypes.object
+  }
+
+  public render() {
+    return (
+      <Group>
+        <Text text="Heythere" listening={false} />
+        </Group>
+    );
+  }
+}
+
+export default CanvasTerrain;
--- src/lib/hexmap/terrain.ts
+++ src/lib/hexmap/terrain.ts
@@ -1,16 +1,17 @@
 // A Hex game terrain
-// import { HexVector } from './hexvector'
+import { HexVector } from './hexvector'
 
 export class Terrain {
   private Name: string
   private Locations: HexVector[]
 
   public name() {
-    return this.Name
+    return this.Name;
   }
 
   public get locations() {
-    return this.locations
+    return this.Locations;
   }
 }
 
+export default Terrain
