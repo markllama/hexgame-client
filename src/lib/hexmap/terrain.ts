7fdc9cc4157453f29d20f7b0f2e4be54458cff6a
Mark Lamourine <markllama@gmail.com>
2018-10-10 17:30:41 -0400

    convert terrain locations to array of HexVector

--- src/lib/hexmap/terrain.ts
+++ src/lib/hexmap/terrain.ts
@@ -4,13 +4,13 @@ import { HexVector } from './hexvector'
 export class Terrain {
   private Name: string
   private Type: string
-  private Locations: Map<string, HexVector>
+  private Locations: HexVector[]
 
   constructor(name: string, type: string) {
     this.Name = name
     this.Type = type
     // this.Locations = new Array<HexVector>()
-    this.Locations = new Map<string, HexVector>()
+    this.Locations = new Array<HexVector>()
   }
 
   public get name() {
