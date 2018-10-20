b5891a1d4819ce954d8e1cf17bf3b64a5e177d3a
Mark Lamourine <markllama@gmail.com>
2018-10-19 21:47:42 -0400

    render two different terrains

--- src/components/CanvasTerrain.tsx
+++ src/components/CanvasTerrain.tsx
@@ -1,7 +1,7 @@
 // import * as Konva from  'konva';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import { Group, Ring } from 'react-konva';
+import { Arc, Group, Ring } from 'react-konva';
 
 import HexVector from '../lib/hexmap/hexvector';
 import Terrain from '../lib/hexmap/terrain';
@@ -21,11 +21,34 @@ class CanvasTerrain extends React.Component<ICanvasTerrain, any> {
   }
 
   public render() {
+
+    let terrainElement:JSX.Element
+
+    if (this.props.terrain.type === 'crater') {
+      terrainElement = this.crater()
+    } else if (this.props.terrain.type === 'hill') {
+      terrainElement = this.hill()
+    } else {
+      terrainElement = <Group />
+    }
+    
     return (
       <Group>
-        <Ring x={this.props.center.hx} y={this.props.center.hy} innerRadius={this.props.radius / 2} outerRadius={this.props.radius * .75} stroke='black' fill='brown' listening={false} />
+        {terrainElement}
       </Group>
     );
+  }
+
+  private crater() {
+    return (
+        <Ring x={this.props.center.hx} y={this.props.center.hy} innerRadius={this.props.radius / 2} outerRadius={this.props.radius * .75} stroke='black' fill='gray' listening={false} />
+    )
+  }
+
+  private hill() {
+    return (
+        <Arc x={this.props.center.hx} y={this.props.center.hy + this.props.radius / 3} angle={120} rotationDeg={210} innerRadius={this.props.radius / 2} outerRadius={this.props.radius * .75} stroke='black' fill='brown' listening={false} />
+    )
   }
 }
 
