23366edd8075f8f2ffa827b29f9f3a83e71e11b7
Mark Lamourine <markllama@gmail.com>
2018-11-02 22:58:40 -0400

    try json with more locations

--- src/lib/hexmap/terrain.test.ts
+++ src/lib/hexmap/terrain.test.ts
@@ -21,12 +21,21 @@ describe('<Terrain>', () => {
   })
 
   it('JSON', () => {
-    const jsonStringT0 = '{"name":"simple"}'
-    const jsonT0 = JSON.parse(jsonStringT0)
     const jsonConvert: JsonConvert = new JsonConvert();
 
+    const jsonStringT0 = '{"name":"simple", "type":"sample"}'
+    const jsonT0 = JSON.parse(jsonStringT0)
     const t0 = jsonConvert.deserializeObject(jsonT0, Terrain)
     expect(t0.name).toEqual("simple")
+    expect(t0.type).toEqual("sample")
     expect(t0.locations.length).toEqual(0)
+
+    const jsonStringT1 = '{"name":"one", "type":"crater", "locations": [{"hx":3, "hy": 4}]}'
+    const jsonT1 = JSON.parse(jsonStringT1)
+    const t1 = jsonConvert.deserializeObject(jsonT1, Terrain)
+    
+    expect(t1.name).toEqual("one")
+    expect(t1.type).toEqual("crater")
+    expect(t1.locations.length).toEqual(1)
   });
 });
