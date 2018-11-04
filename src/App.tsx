9eefa75dfe34df9282e21cc7933c4b964d623857
Mark Lamourine <markllama@gmail.com>
2018-11-03 23:13:18 -0400

    start reading map data from query json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { JsonConvert } from 'json2typescript'
+
 import * as React from 'react';
+
 import './App.css';
 
 import logo from './images/one_hex.png';
 
 import { HexmapCanvas, Orientation } from "./components/HexmapCanvas";
-import HexVector from "./lib/hexmap/hexvector";
+// import HexVector from "./lib/hexmap/hexvector";
 import HexMap from "./lib/hexmap/map";
-import Terrain from "./lib/hexmap/terrain";
+// import Terrain from "./lib/hexmap/terrain";
+
 
 class App extends React.Component {
 
@@ -27,31 +31,48 @@ class App extends React.Component {
   }
 
   private createMap():HexMap {
-    const size = new HexVector(15,22);
-    const hm = new HexMap("testmap", "testgame", size);
 
-    const hills = new Terrain("hills", "hill", new Array<HexVector>())
-    hills.locations.push(new HexVector(0, 0))
-    hills.locations.push(new HexVector(6, 17))
-    hills.locations.push(new HexVector(12, 24))
-    hills.locations.push(new HexVector(10, 14))
-    hills.locations.push(new HexVector(4, 22))
+    const req = new XMLHttpRequest()
+    const mapUrl = "http://localhost:3001/samplemap.json"
+
+    let hm:HexMap = new HexMap()
+
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        const mapObject = JSON.parse(req.responseText);
+        const jsonConvert: JsonConvert = new JsonConvert();
+        hm = jsonConvert.deserialize(mapObject, HexMap)
+      }
+    }
+    req.open("GET", mapUrl, true)
+    req.send()
+
+    // const size = new HexVector(15,22);
+    // const hm = new HexMap("testmap", "testgame", size);
+
+    // const hills = new Terrain("hills", "hill", new Array<HexVector>())
+    // hills.locations.push(new HexVector(0, 0))
+    // hills.locations.push(new HexVector(6, 17))
+    // hills.locations.push(new HexVector(12, 24))
+    // hills.locations.push(new HexVector(10, 14))
+    // hills.locations.push(new HexVector(4, 22))
     
-    const craters = new Terrain("craters", "crater")
-    craters.locations.push(new HexVector(14, 9))
-    craters.locations.push(new HexVector(12, 4))
-    craters.locations.push(new HexVector(13, 18))
-    craters.locations.push(new HexVector(9, 21))
-    craters.locations.push(new HexVector(7 , 14))
-    craters.locations.push(new HexVector(3 , 20))
-    craters.locations.push(new HexVector(6, 6))
+    // const craters = new Terrain("craters", "crater")
+    // craters.locations.push(new HexVector(14, 9))
+    // craters.locations.push(new HexVector(12, 4))
+    // craters.locations.push(new HexVector(13, 18))
+    // craters.locations.push(new HexVector(9, 21))
+    // craters.locations.push(new HexVector(7 , 14))
+    // craters.locations.push(new HexVector(3 , 20))
+    // craters.locations.push(new HexVector(6, 6))
 
-    hm.terrains.add(hills)
-    hm.terrains.add(craters)
+    // hm.terrains.add(hills)
+    // hm.terrains.add(craters)
     
     return hm
   }
 
 }
+
 
 export default App;
