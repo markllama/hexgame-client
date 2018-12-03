3332685d5d3e5a6dffb2c9ceb9310f82a580aeaf
Mark Lamourine <markllama@gmail.com>
2018-12-02 20:39:08 -0500

    add routing for single-page app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
-import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, NavLink, Route, } from "react-router-dom";
 // import { NavLink, Route, HashRouter as Router } from "react-router-dom";
 
-import { HexmapCanvas, Orientation } from "./components/HexmapCanvas";
+import Dashboard from "./components/Dashboard";
+import { HexmapCanvas } from "./components/HexmapCanvas";
 
 import './App.css';
 
@@ -13,31 +15,41 @@ import logo from './images/one_hex.png';
 class App extends React.Component {
 
   public render() {
+
+    const hm = () => <HexmapCanvas hexmapurl="./samplemap.json" />
+      
     return (
-      <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to the Hexgame Server</h1>
-        </header>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h1 className="App-title">Welcome to the Hexgame Server</h1>
+            </header>
 
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            <Button>
-            <Typography variant="h6" color="secondary">
-            File
-            </Typography>
-            </Button>
-            <Typography variant="h6" color="inherit">
-            Edit
-            </Typography>
-          </Toolbar>
-        </AppBar>
+            <AppBar position="static" color="default">
+              <Toolbar>
+                <IconButton className="menuButton" color="inherit" aria-label="Open drawer">
+                  <MenuIcon />
+                </IconButton>
+                <Typography className="menu" variant="h6" color="inherit" noWrap={true}>
+                Hexgame Server
+                </Typography>
+                &nbsp;
+                <Typography variant="h6" color="inherit">
+                  <NavLink to="/dashboard">Home</NavLink>
+                </Typography>
+                &nbsp;
+                <Typography variant="h6" color="inherit">
+                  <NavLink to="/map">Map</NavLink>
+                </Typography>
+              </Toolbar>
+            </AppBar>
 
-        <div>
-          <HexmapCanvas hexmapurl={"./samplemap.json"} orientation={Orientation.Landscape} hexrun={30}/>
-        </div>
-        </div>
+            <div className="content">
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/map" render={hm} />
+            </div>
+          </div>
       </Router>
     );
   }
